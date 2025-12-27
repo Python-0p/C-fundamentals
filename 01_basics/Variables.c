2588e9dab590b17a138b65db2d1143af5657ff46
Pratham Srivastava <9696801020p69@gmail.com>
2025-12-28 00:39:12 +0530

    Create Variables.c

---
+++ 01_basics/Variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+int a = 10; 
+float b = 1.1;
+char c = 'a';
+double d = 12.3;
+
+printf("%d This is Integer.\n"); //%d is Format Specifier For Int.
+printf("%f This is float.\n");//%f is Format Specifier For Float.
+printf("%c This is Char.\n");//%c is Format Specifier For Char.
+printf("%lf This is Double.\n");//%lf is Format Specifier For double.
+
+
+
+return 0;
+}
