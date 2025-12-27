248aa9377cb523afc4dff2d2d0ca81fc7539bbe7
Pratham Srivastava <9696801020p69@gmail.com>
2025-12-28 00:41:50 +0530

    Update Variables.c

--- 01_basics/Variables.c
+++ 01_basics/Variables.c
@@ -7,10 +7,10 @@ float b = 1.1;
 char c = 'a';
 double d = 12.3;
 
-printf("%d This is Integer.\n"); //%d is Format Specifier For Int.
-printf("%f This is float.\n");//%f is Format Specifier For Float.
-printf("%c This is Char.\n");//%c is Format Specifier For Char.
-printf("%lf This is Double.\n");//%lf is Format Specifier For double.
+printf("%d This is Integer.\n", a); //%d is Format Specifier For Int.
+printf("%.2f This is float.\n", b);//%f is Format Specifier For Float.
+printf("%c This is Char.\n", c);//%c is Format Specifier For Char.
+printf("%.2lf This is Double.\n", d);//%lf is Format Specifier For double.
 
 
 
