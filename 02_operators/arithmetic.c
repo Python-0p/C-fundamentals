930729f5301ea206f904575073a45fce25865c3f
Pratham Srivastava <9696801020p69@gmail.com>
2025-12-28 00:51:04 +0530

    Create arithmetic.c

---
+++ 02_operators/arithmetic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 10, b = 20;
+
+    int Sum = a+b;
+    int Sub = a-b;
+    int Multiplication = a*b;
+    int divide = a/b;
+
+    printf("%d is Sum.\n", Sum);
+    printf("%d is Sub.\n", Sub);
+    printf("%d is Multiplication.\n", Multiplication);
+    printf("%d is Divide.\n", divide);
+
+    return 0;
+}
