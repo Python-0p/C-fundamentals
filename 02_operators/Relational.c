11784b0cb903c2e27dca1e87c081c458607191cd
Pratham Srivastava <9696801020p69@gmail.com>
2026-01-04 11:19:21 +0530

    Implement relational operator example in Relational.c
    Added a program to demonstrate relational operators in C.

---
+++ 02_operators/Relational.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+int a = 1, b = 2;
+
+int c = a>b; // Here c has a Value which is decided by comparing of a and b.
+printf("%d\n", c);
+/* output will be 0 bcuz A is not greater than B
+thier are Multiple Relational Operators we Can use in C language like 
+==, >, <, <=, >=, !=
+all of them are used to compare between Variables and give result between 0 and 1.*/
+return 0;
+}
